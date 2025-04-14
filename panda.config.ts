d65d45ed45bb5675874e589675619a2aa7d255f9
airRnot1106 <yngw.rintaro@gmail.com>
2025-04-15 00:36:56 +0900

    env: add font-size tokens

--- panda.config.ts
+++ panda.config.ts
@@ -12,7 +12,24 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        fontSizes: {
+          xs: { value: '0.75rem' },
+          sm: { value: '0.875rem' },
+          md: { value: '1rem' },
+          lg: { value: '1.125rem' },
+          xl: { value: '1.25rem' },
+          '2xl': { value: '1.5rem' },
+          '3xl': { value: '1.875rem' },
+          '4xl': { value: '2.25rem' },
+          '5xl': { value: '3rem' },
+          '6xl': { value: '4rem' },
+          '7xl': { value: '5rem' },
+          '8xl': { value: '6rem' },
+        },
+      },
+    },
   },
 
   // The output directory for your css system

931845fc70a3520a24cc666ed3bbf43da102c588
airRnot1106 <airRnot1106@users.noreply.github.com>
2025-05-01 06:12:19 +0000

    deploy: 44163796683cdb76104cc2329f71ee0e5bf7dbc0

---
+++ assets/site-layout.stories-DSQyDCyW.js
@@ -0,0 +1,55 @@
+import{j as e}from"./jsx-runtime-DoEZbXM1.js";import{c as R}from"./cx-CjKSjrk3.js";import{S as T}from"./site-footer-DApwN5MT.js";import{S as D}from"./site-header-CoNGFsqt.js";import"./jsx-runtime-Bw5QeaCk.js";import"./link-button-DbYqTAs9.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./add-base-path-C6R_RFSA.js";import"./index-Za3YRiuF.js";import"./button-DHkYrrPp.js";import"./ghost-button-DmZE5ofc.js";import"./button-C3-eB2TV.js";import"./cva-B6Wyu4tH.js";import"./normal-button-Coi8qj4b.js";import"./index-SP2qOBwU.js";import"./link-button-DQg4kryY.js";import"./ghost-link-button-Cr2qyDpj.js";import"./normal-link-button-DINgtC2G.js";import"./site-theme-toggle-button-DlKrcEvn.js";import"./panic-helper-BEWS6D6P.js";import"./contexts-CBUbIPL_.js";const m=({children:X,className:_,...N})=>e.jsxs("div",{className:R({display:"grid",gridTemplateRows:"auto 1fr auto",minHeight:"100dvh"}),...N,children:[e.jsx(D,{}),e.jsx("div",{className:_,children:X}),e.jsx(T,{})]});m.__docgenInfo={description:"",methods:[],displayName:"SiteLayout",props:{children:{required:!0,tsType:{name:"ReactNode"},description:""}}};const re={title:"Features/Site/Molecules/SiteLayout",component:m,tags:["autodocs"],parameters:{layout:"fullscreen"}},E=()=>e.jsxs("main",{children:[e.jsx("h1",{children:"Site Layout"}),e.jsx("p",{children:"This is the main content area."})]}),r=e.jsx(m,{children:e.jsx(E,{})}),t={render:()=>r},o={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xs"}},render:()=>r},a={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"sm"}},render:()=>r},s={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"md"}},render:()=>r},n={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"lg"}},render:()=>r},p={tags:["!autodocs"],parameters:{viewport:{defaultViewport:"xl"}},render:()=>r};var i,d,c;t.parameters={...t.parameters,docs:{...(i=t.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  render: () => {
+    return Component;
+  }
+}`,...(c=(d=t.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var u,l,g;o.parameters={...o.parameters,docs:{...(u=o.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xs'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(g=(l=o.parameters)==null?void 0:l.docs)==null?void 0:g.source}}};var w,f,S;a.parameters={...a.parameters,docs:{...(w=a.parameters)==null?void 0:w.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'sm'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(S=(f=a.parameters)==null?void 0:f.docs)==null?void 0:S.source}}};var x,v,h;s.parameters={...s.parameters,docs:{...(x=s.parameters)==null?void 0:x.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'md'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(h=(v=s.parameters)==null?void 0:v.docs)==null?void 0:h.source}}};var j,V,y;n.parameters={...n.parameters,docs:{...(j=n.parameters)==null?void 0:j.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'lg'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(y=(V=n.parameters)==null?void 0:V.docs)==null?void 0:y.source}}};var L,C,M;p.parameters={...p.parameters,docs:{...(L=p.parameters)==null?void 0:L.docs,source:{originalSource:`{
+  tags: ['!autodocs'],
+  parameters: {
+    viewport: {
+      defaultViewport: 'xl'
+    }
+  },
+  render: () => {
+    return Component;
+  }
+}`,...(M=(C=p.parameters)==null?void 0:C.docs)==null?void 0:M.source}}};const te=["Default","XSmall","Small","Medium","Large","XLarge"];export{t as Default,n as Large,s as Medium,a as Small,p as XLarge,o as XSmall,te as __namedExportsOrder,re as default};
